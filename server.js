23649ee4251161fe3b316e73ace598caf9810ed2
Lucas <lucassdeabreu@gmail.com>
2018-01-23 21:36:47 -0200

    heroku

--- server.js
+++ server.js
@@ -3,6 +3,6 @@ var path = require('path')
 var serveStatic = require('serve-static')
 app = express();
 app.use(serveStatic(__dirname + "/dist"))
-var port = process.env.PORT || 5001;
-app.listen(port);
-console.log('server started ' + port)
+app.listen(process.env.PORT || 3000, function() {
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env)
+})
