43c1317d4ffb8e7417ab9c28715ae6bf5616123e
Lucas <lucassdeabreu@gmail.com>
2018-02-08 21:57:09 -0200

    error in commir

---
+++ src/components/globals/button.vue
@@ -0,0 +1,14 @@
+<template>
+  <button type="submit" class="waves-effect waves-light btn-small btn" @click="action"><slot></slot></button>
+</template>
+
+<script>
+export default {
+  props: ['name'],
+  methods: {
+    action () {
+      this.$emit('click')
+    }
+  }
+}
+</script>
